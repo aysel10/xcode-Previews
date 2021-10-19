e1d9b97859bab3ada6e1dfff3cf8e7500bcb5d38
Aysel Abdullayeva <aysel2920@gmail.com>
2021-10-19 22:35:35 +0200

    Initial Commit

---
+++ xcode-Previews/xcode_PreviewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  xcode_PreviewsApp.swift
+//  xcode-Previews
+//
+//  Created by icelkas on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct xcode_PreviewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
